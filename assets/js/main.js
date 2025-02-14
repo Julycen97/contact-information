aa00be45e9c0722f2965936531751ef6a3680a16
Julian Centurion <july_cen@hotmail.com.ar>
2025-02-14 17:47:01 -0300

    cambio en footer con js y en index en cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const footerText = document.querySelector("#footerText");
     const imgHome = document.querySelector(".imgHome");
     const imagenes = ["./assets/images/publicidad1.png", "./assets/images/publicidad2.png", "./assets/images/publicidad3.png"];
     let indice = 0;
   
+    footerText.innerHTML = `&copy; ${new Date().getFullYear()} - Creado por <a href="https://www.linkedin.com/in/julian-centurion/" target="_blank" rel="noopener noreferrer">Julián Centurión</a>`;
+
     setInterval(() => {
       imgHome.style.backgroundImage = `url(${imagenes[indice]})`;
 
